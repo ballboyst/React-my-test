6fea5a1845ca8dce8bc5bc6f55b01ccd8b4aab00
羽田　有希 <naoki@hatayukinoMacBook-Pro.local>
2024-06-02 16:09:44 +0900

    31.JSX内でJSコードを実行してみよう

--- my-test-app/src/App.js
+++ my-test-app/src/App.js
@@ -1,13 +1,12 @@
 import Child from './components/Child';
-const Example = () => {
-    return (
-      <div>
-        <h3>練習問題</h3>           
-        <p>/componentsフォルダをstart内に作成して、Childにコンポーネントを分離して、Exampleコンポーネントで読み込んでください。</p>
-        <Child />
-      </div>
-    );
-  };
-  
-  export default Example;
-  
+import Expression from './components/Expression';
+
+const Example = () => (
+    <>
+    <Child/>
+    <p></p>
+    <Expression/>
+    </>
+);
+
+export default Example;
--- my-test-app/src/components/Child.js
+++ my-test-app/src/components/Child.js
@@ -5,14 +5,9 @@ console.log(React)
 
 const Child = () => {
     return (
-        <React.Fragment>
             <div className="component">
                 <h3>Hello Component</h3>
             </div>
-                <h3>Hello Fragment</h3>
-                <p>Lorem etc</p>
-                {/* <List /> */}
-        </React.Fragment>
     );
 };
 
