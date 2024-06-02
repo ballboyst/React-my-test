c0a98ccde1b51bde27f19ed464c170d1f544e366
羽田　有希 <naoki@hatayukinoMacBook-Pro.local>
2024-06-02 14:48:29 +0900

    28.コンポーネントの分割方法

---
+++ my-test-app/src/components/Child.js
@@ -0,0 +1,12 @@
+import "./Child.css"
+import { List } from "./list"
+const Child = () => {
+    return (
+        <div className="component">
+            <h3>Hello</h3>
+            <List />
+        </div>
+    );
+};
+
+export default Child;
