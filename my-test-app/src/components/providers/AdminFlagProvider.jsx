7285f9ed4a131f08f21d8008c702e970509cc90d
羽田　有希 <naoki@hatayukinoMacBook-Pro.local>
2024-06-13 21:18:30 +0900

    ended  p187

--- my-test-app/src/components/providers/AdminFlagProvider.jsx
+++ my-test-app/src/components/providers/AdminFlagProvider.jsx
@@ -3,14 +3,13 @@ import { createContext, useState } from "react" ;
 export const AdminFlagContext = createContext({});
 
 export const AdminFlagProvider = props => {
-const { children } = props;
+    const { children } = props;
 
-    const [isAdmin, setIsAdmin] = useState(false);
-
-    // const sampleObj = { sampleValue: "テスト" };
+// 動作確認のために適当なオブジェクトを定義
+    const sampleObj = { sampleValue: "テスト" };
 
     return (
-        <AdminFlagContext.Provider value={{ sampleObj }}>
+        <AdminFlagContext.Provider value={ sampleObj }>
             {children}
         </AdminFlagContext.Provider>
     );
