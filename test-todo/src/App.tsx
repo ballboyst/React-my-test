d39907718f92fdd4d67976ef10bccf9c9b809186
ballboyst <ballboyst365@gmail.com>
2024-06-16 06:59:28 +0900

    create test-todo

--- test-todo/src/App.tsx
+++ test-todo/src/App.tsx
@@ -1,5 +1,5 @@
-import { FC, useState } from "react"
-
+import { ChangeEvent, FC, useState } from "react"
+import styled from "styled-components";
 // export const App = () => {
 //     return <h1>簡単メモアプリ</h1>
 // }
@@ -8,33 +8,36 @@ import { FC, useState } from "react"
 export const App: FC = () =>{
     // テキストボックスのStateを定義
     const [text,setText] = useState<string>("");
-    // アロー関数でAppメソッドの処理を定義（FCは関数コンポーネントを型定義している）
+    // 解説：アロー関数でAppメソッドの処理を定義（FCは関数コンポーネントを型定義している）
     // 処理内容は次のとおり。
     // string型に指定されたuseStateメソッドで初期値""をState変数textに代入。setTextはtextの状態更新用。
     // つまり、分割代入で右辺のuseState("")を左辺の配列内textに代入しているということ。右辺の方が要素数が多いためsetTextはundefindが代入される。
     const [memos, setMemos] = useState<string[]>([]);
-    // メモリストは文字列型の配列なのでuseState<string[]>となる。よって初期値も空配列[]で与える。
-
+    // 解説：メモリストは文字列型の配列なのでuseState<string[]>となる。よって初期値も空配列[]で与える。
     // テキストボックスに入力された時、変数を更新する。
     const onChangeText = (e:ChangeEvent<HTMLInputElement>) =>setText(e.target.value);
-    // onChangeTextにアロー関数を定義。アロー関数の中身は以下。
+    // 解説：onChangeTextにアロー関数を定義。アロー関数の中身は以下。
     // 引数(e:ChangeEvent<HTMLInputElement>)はイベントeの型を定義している。
     // setText(e.target.value)は引数の値を使って変数を更新する。つまり、変数textをイベントの発生元であるtarget.valueに更新する。
     // アロー関数の処理が{}で囲まれていないのは式が１つしかないため省略されている。
     const onClickAdd = () => {
         const newMemos =  [...memos];
-        // スプレッド構文で配列memosをコピーして新しい配列newMemosを作成
+        // 解説：スプレッド構文で配列memosをコピーして新しい配列newMemosを作成
         newMemos.push(text);
-        // newMemosにpushメソッドでtextを追加
+        // 解説：newMemosにpushメソッドでtextを追加
         setMemos(newMemos);
-        // State更新用のsetMemosは引数としてnewMemosを受け取り、memosを更新する。
+        // 解説：State更新用のsetMemosは引数としてnewMemosを受け取り、memosを更新する。
         // 結果、Reactが更新を検知しUIを再レンダリングし画面に反映する。
         setText("");
-        // 状態変数更新用のsetTextに空文字をセットする（つまりリセットする）。
+        // 解説：状態変数更新用のsetTextに空文字をセットする（つまりリセットする）。
     };
     
+    const onClickDelete = (index: number) => {
+      const newMemos = [...memos];
+      newMemos.splice(index,1);
+      setMemos(newMemos);
+    }
     
-    // ここに削除ボタン押下時の状態管理処理を書く
     
 
     return (
