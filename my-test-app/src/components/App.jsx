1ca0646a3e697f85f40e395171bdfce776ed29a1
ballboy <ballboyst365@gmail.com>
2024-06-29 06:49:53 +0900

    second

--- my-test-app/src/components/App.jsx
+++ my-test-app/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {useState} from "react";
-
+import styled from "styled-components";
 
 export const App = () => {
     const [text,setText] = useState("");
@@ -8,21 +8,50 @@ export const App = () => {
     const onChangeText = (e) => setText(e.target.value);
     const onClickAdd = () => {
         const newMemos = [...memos];
-        newMemos.push(setText);
+        newMemos.push(text);
         setMemos(newMemos);
         setText("");
-        }
+        };
     return (
-        <div>
+        <SDiv>
             <h1>簡単メモアプリ</h1>
             <input type="text" value={text} onChange={onChangeText}></input>
-            <buttun onClickAdd={onClickAdd}>送信</buttun>
-            <div>
-                <p>memos</p>
-            </div>
-        </div>
+            <SButton onClick={onClickAdd}>追加</SButton>
+            <SMemo>
+                <p>メモリスト</p>
+                <div>
+                    <ul>
+                        {
+                        memos.map(
+                            (memo)=>(
+                            <li key={memo}>
+                            <p>{memo}</p>
+                            </li>
+                                )
+                            )
+                        }
+                    </ul>
+                </div>
+            </SMemo>
+        </SDiv>
 
     )
 };
 
 export default App;
+
+const SDiv = styled.div`
+    background-color: #339999;
+    text-align: center;
+`;
+const SButton = styled.button`
+    border-radius: 5px;
+    border: 1px solid;
+    margin: 5px;
+    height: 30px;
+`
+const SMemo = styled.div`
+    font-size: 16px;
+    border: solid 2px black;
+    border-radius: 5px;
+`;
