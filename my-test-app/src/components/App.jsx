738b08b70a5d09ac9c4dc03621dceb295c04c968
ballboy <ballboyst365@gmail.com>
2024-06-30 06:26:53 +0900

    first

--- my-test-app/src/components/App.jsx
+++ my-test-app/src/components/App.jsx
@@ -1,68 +1,36 @@
-import React from "react";
-import {useState} from "react";
-import styled from "styled-components";
+import React, { memo } from "react";
+import { useState } from "react";
+// import styled from "styled-components"
 
-export const App = () => {
+const App = () => {
     const [text,setText] = useState("");
     const [memos,setMemos] = useState([]);
-    const onChangeText = (e) => setText(e.target.value);
+
+    const onChangeText = (e) => {setText(e.target.value)
+    };
+
     const onClickAdd = () => {
         const newMemos = [...memos];
+        setText={text};
         newMemos.push(text);
         setMemos(newMemos);
-        setText("");
-        };
-    const onClickDelete = (index) => {
-        const newMemos=[...memos];
-        newMemos.splice(index,1);
-        setMemos(newMemos);
-    }
-    return (
-        <SDiv>
-            <h1>簡単メモアプリ</h1>
-            <input type="text" value={text} onChange={onChangeText}></input>
-            <SButton onClick={onClickAdd}>追加</SButton>
-            <SMemo>
-                <p>メモリスト</p>
-                <div>
-                    <ul>
-                        {
-                        memos.map(
-                            (memo,index)=>(
-                            <li key={memo}>
-                            <SContainer>
-                            <p>{memo}</p>
-                            <SButton onClick={()=>onClickDelete(index)}>削除</SButton>
-                            </SContainer>
-                            </li>
-                                )
-                            )
-                        }
-                    </ul>
-                </div>
-            </SMemo>
-        </SDiv>
+    };
 
+    return(
+    <div>
+    <h1>簡単メモリスト</h1>
+    <input type="text" value={text} onChange={onChangeText}></input>
+    <button onClick={onClickAdd}>追加</button>
+    <p>メモリスト</p>
+    <ul>{
+        memos.map(
+            (memo)=>{
+                <li>{memo}</li>
+            }
+        )}
+    </ul>
+    </div>
     )
 };
 
-export default App;
-
-const SDiv = styled.div`
-    background-color: #339999;
-    text-align: center;
-`;
-const SButton = styled.button`
-    border-radius: 5px;
-    border: 1px solid;
-    margin: 5px;
-    height: 30px;
-`
-const SMemo = styled.div`
-    font-size: 16px;
-    border: solid 2px black;
-    border-radius: 5px;
-`;
-const SContainer = styled.div`
-    display: flex;
-`;
+export default(App);
