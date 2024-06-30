f779ba68ee4fe19c2fe63c40a5f2392481d8465a
ballboy <ballboyst365@gmail.com>
2024-06-30 22:57:35 +0900

    props修正中

--- my-test-app/src/components/App.jsx
+++ my-test-app/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
-// import styled from "styled-components"
+import styled from "styled-components"
+import { MemoList } from "./MemoList";
 
 const App = () => {
     const [text,setText] = useState("");
@@ -15,21 +16,45 @@ const App = () => {
         setMemos(newMemos);
     };
 
+    const onClickDelete = (index)=> {
+        const newMemos = [...memos];
+        newMemos.splice(index, 1);
+        setMemos(newMemos);
+    }
+
     return(
-    <div>
+    <SDiv>
     <h1>簡単メモリスト</h1>
     <input type="text" value={text} onChange={onChangeText}></input>
-    <button onClick={onClickAdd}>追加</button>
+    <SButton onClick={onClickAdd}>追加</SButton>
     <p>メモリスト</p>
-    <ul>{
+    <MemoList memos={memos} onClick={onClickDelete} />
+    {/* <ul>{
         memos.map(
-            (memo)=>(
+            (memo, index)=>(
+                <SMemos>
                 <li>{memo}</li>
+                <SButton key={index} onClick={onClickDelete}>削除</SButton>
+                </SMemos>
             )
         )}
-    </ul>
-    </div>
+    </ul> */}
+    </SDiv>
     )
 };
+
+const SDiv =  styled.div`
+    background-color: green;
+    text-align: center;
+`;
+const SButton = styled.button`
+    border: solid 1px #ccc;
+    border-radius: 5px;
+    margin: 5px;
+`;
+// const SMemos = styled.div`
+//     display:flex;
+// `;
+
 
 export default(App);
---
+++ my-test-app/src/components/MemoList.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { useState } from "react";
+
+
+
+export const MemoList = (props) =>{
+    const [memos,setMemos] = useState([]);
+    const onClickDelete = (index)=> {
+    const newMemos = [...memos];
+    newMemos.splice(index, 1);
+    setMemos(newMemos);
+    }
+    return(
+    <ul>{
+        memos.map(
+            (memo, index)=>(
+                <SMemos>
+                <li>{memo}</li>
+                <SButton key={index} onClick={onClickDelete}>削除</SButton>
+                </SMemos>
+            )
+        )}
+    </ul>
+    )};
+
+const SButton = styled.button`
+    border: solid 1px #ccc;
+    border-radius: 5px;
+    margin: 5px;
+`;
+const SMemos = styled.div`
+    display:flex;
+`;
