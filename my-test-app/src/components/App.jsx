6a47e5947055ef49eacd3ae84fe6e7a56e45971e
ballboy <ballboyst365@gmail.com>
2024-07-03 16:16:46 +0900

    レンダリング調整中

---
+++ my-test-app/src/components/App.jsx
@@ -0,0 +1,64 @@
+import React from "react"
+import {useState} from "react"
+import styled from "styled-components"
+
+const App = () => {
+    const [text, setText]=useState("");
+    const [memos, setMemos]=useState([]);
+
+    const onChangeText = (e) => setText(e.target.value);
+    const onClickAdd = () => {
+        const newMemos = [...memos];
+        newMemos.push(text);
+        setMemos(newMemos);
+        setText("");
+    };
+    const onClickDelete = (index) =>{
+        const newMemos =  [...memos];
+        newMemos.splice(index, 1);
+        setMemos(newMemos);
+    };
+
+
+
+    return(
+        <SDiv>
+            <h1>簡単メモ</h1>
+            <SContainer>
+                <input type="text" value={text} onChange={onChangeText}></input>
+                <SButton onClick={onClickAdd}>追加</SButton>
+            </SContainer>
+            <p>メモリスト</p>
+            <ul>
+                {memos.map(
+                    (memo, index)=>(
+                        <li key={memo}>
+                            <SContainer>
+                                <p>{memo}</p>
+                                <SButton onClick={onClickDelete(index)}>削除</SButton>
+                            </SContainer>
+                        </li>
+                    ))}
+            </ul>
+        </SDiv>
+    )
+
+};
+
+
+const SDiv = styled.div`
+    background-color: green;
+    text-align: center;
+`;
+const SContainer = styled.div`
+    display: flex;
+    margin-left: 40%;
+
+`;
+const SButton = styled.button`
+    border: solid 1px;
+    border-radius: 5px;
+    margin: 5px;
+`;
+
+export default(App);
